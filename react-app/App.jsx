bc659b733e83acc7e32753efd2ebab7a3b9e9cb1
Jhon Vente <jhon.vente@edunext.co>
2023-09-04 11:03:31 -0500

    fix: class name for main div

--- react-app/App.jsx
+++ react-app/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="test-div">
-      <h1 className="title">Images  Gallery XBlock</h1>
+    <div className="container">
+      <h1 className="title">Images Gallery XBlock</h1>
     </div>
   );
 };
