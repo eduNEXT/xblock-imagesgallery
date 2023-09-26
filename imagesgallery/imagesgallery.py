50dbab4ec288eed8bddb17669226867f282a2a50
Maria Grimaldi <maria.grimaldi@edunext.co>
2023-09-26 13:33:19 -0400

    fix: go back to import inside methods

--- imagesgallery/imagesgallery.py
+++ imagesgallery/imagesgallery.py
@@ -28,16 +28,6 @@ except ImportError:
     contentstore = None
     AssetKey = None
 
-# Temporary fix for supporting both contentstore assets management versions (master / Palm)
-try:
-    from cms.djangoapps.contentstore.views.assets import update_course_run_asset, delete_asset
-except ImportError:
-    from cms.djangoapps.contentstore.asset_storage_handler import update_course_run_asset, delete_asset
-except ModuleNotFoundError:
-    # Avoid errors while running tests
-    update_course_run_asset = None
-    delete_asset = None
-
 log = logging.getLogger(__name__)
 
 
@@ -221,6 +211,11 @@ class ImagesGalleryXBlock(XBlock):
     @XBlock.handler
     def file_upload(self, request, suffix=''):  # pylint: disable=unused-argument
         """Handler for file upload to the course assets."""
+        # Temporary fix for supporting both contentstore assets management versions (master / Palm)
+        try:
+            from cms.djangoapps.contentstore.views.assets import update_course_run_asset  # pylint: disable=import-outside-toplevel
+        except ImportError:
+            from cms.djangoapps.contentstore.asset_storage_handler import update_course_run_asset  # pylint: disable=import-outside-toplevel
         uploaded_content = []
         for _, file in request.params.items():
             try:
@@ -257,6 +252,10 @@ class ImagesGalleryXBlock(XBlock):
     @XBlock.json_handler
     def remove_files(self, data, suffix=''):  # pylint: disable=unused-argument
         """Handler for removing images from the course assets."""
+        try:
+            from cms.djangoapps.contentstore.views.assets import delete_asset  # pylint: disable=import-outside-toplevel
+        except ImportError:
+            from cms.djangoapps.contentstore.asset_storage_handler import delete_asset  # pylint: disable=import-outside-toplevel
         asset_key = AssetKey.from_string(data.get("asset_key"))
         try:
             delete_asset(self.course_id, asset_key)
